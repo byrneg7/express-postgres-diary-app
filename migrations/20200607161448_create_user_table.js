b73830c8159c82f1eaa610ad62c8e04c5ef22a88
byrneg7 <greg@showoff.ie>
2020-06-07 16:52:21 +0100

    knex+bookshelf+password - work on auth

---
+++ migrations/20200607161448_create_user_table.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('login_user', t => {
+    t.increments('id').unsigned().primary();
+    t.string('email').notNullable();
+    t.string('password_digest').notNullable()
+  })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('login_user');
+};
